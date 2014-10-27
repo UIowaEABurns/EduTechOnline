75b4e809d096ee44e7225073a4603fbe847268c8
Eric Burns <eric-burns@uiowa.edu>
2014-10-27 18:50:49 -0500

    Fixing compile errors

--- src/edutechonline/servlets/CoursesAdd.java
+++ src/edutechonline/servlets/CoursesAdd.java
@@ -43,12 +43,7 @@ import edutechonline.util.Validator;
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
